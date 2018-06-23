98bde543c3483294d7dcdeb05c6ce5d8e7dc5c38
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-06-23 17:55:57 +0100

    testing all CRUD operations

--- playground/mongo-test.js
+++ playground/mongo-test.js
@@ -15,22 +15,54 @@ MongoClient.connect('mongodb://localhost:27017/TodosApp',(err , client) => {
     //Chose the database
     const db = client.db('TodosApp');
 
-
+    /* Create */
     // db.collection('Todos').insertOne({
     //     title : "fourth todo title",
-    //     body : "fourth todo body"
+    //     body : "fourth todo body",
+    //     num : 16
     // },(err, result) => {
     //     if(err){
     //         return console.log('Can\'t insert this todo',err);
     //     }
     //     console.log('Inserted with success ! ');
     // })
-    var id = new ObjectID('5b2e23a089ab3e19bd229f4a');
-    db.collection('Todos').count().then((count) => {
-        console.log('All Todos number');
-        console.log(count)
-    },(err)=>{
-        console.log('Cant fetching data',err);
+
+    /* Count */
+    //var id = new ObjectID('5b2e23a089ab3e19bd229f4a');
+    // db.collection('Todos').count().then((count) => {
+    //     console.log('All Todos number');
+    //     console.log(count)
+    // },(err)=>{
+    //     console.log('Cant fetching data',err);
+    // });
+
+    /* Delete */
+     var id = new ObjectID('5b2e79a52d52540bd2fd49c8');
+    // db.collection('Todos').findOneAndDelete({_id : id}).then((result) => {
+    //     console.log("Deleted !");
+    //     console.log(result.ok);
+    // },(err) => {
+    //     cons   ole.log('Cant delete that');
+    //     console.log(err);
+    // });
+
+
+    /* Update */
+    db.collection('Todos').findOneAndUpdate(
+        {_id : id},
+        {
+            $set:{
+                title : "updated"
+            },
+            $inc:{
+                num : 2
+            }
+        }
+    ).then((result) => {
+        console.log("Updated with success !");
+        console.log(result);
+    },(err) => {
+        console.log(err);
     });
     client.close();
 });
